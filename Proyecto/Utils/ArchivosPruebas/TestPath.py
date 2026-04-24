c624820683d7d043f72cfc2ec6acb16204d1feb6
M4FB <marcelo.avendano@ucsm.edu.pe>
2026-04-23 19:03:03 -0500

    Prueba Modularizada

--- Proyecto/Utils/ArchivosPruebas/TestPath.py
+++ Proyecto/Utils/ArchivosPruebas/TestPath.py
@@ -1,3 +1,5 @@
+# Crea Carpetas, y las elimina luego de 5 segundos
+
 from pathlib import Path
 from time import sleep
 p = Path('./Proyecto/Utils/ArchivosPruebas')
@@ -8,9 +10,40 @@ for i in range(1,10):
     nueva_carpeta = p / str(i)
     nueva_carpeta.mkdir(mode=0o777, parents=False,exist_ok=True)
 
+    for j in range(1,10):
+        nuevo_archivo = nueva_carpeta /  ( str(j) + ".txt" )
+        nuevo_archivo.touch(0o755,True)
 
-sleep(5)
 
-for i in range(1,10):
-    vieja_carpeta = p / str(i)
-    vieja_carpeta.rmdir()
+
+def crearDirectoriosEmergentes():
+    for i in range(1,10):
+        nueva_carpeta = p / str(i)
+        nueva_carpeta.mkdir(mode=0o777, parents=False,exist_ok=True)
+
+def eliminarDirectoriosEmergentes():
+    for i in range(1,10):
+        carpeta_usada = p / str(i)
+        carpeta_usada.rmdir()
+
+def crearArchivosEmergentes():
+    for i in range(1,10):
+        archivo = p / str (i) / f"Tesis{i}.pdf"
+        archivo.touch(755,True)
+
+def eliminarArchivosEmergentes():
+    for i in range(1,10):
+        for j in range(1,10):
+            archivo = p / str (i) / f"Tesis{j}.pdf"
+            archivo.unlink(missing_ok=True)
+
+            archivo = p / str (i) / f"{j}.txt"
+            archivo.unlink(missing_ok=True)
+
+            archivo = p / str (i) / f"{j}"
+            archivo.unlink(missing_ok=True)
+
+if __name__ == "__main__":
+    eliminarArchivosEmergentes()
+
+    
